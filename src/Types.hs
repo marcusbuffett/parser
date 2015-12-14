5d35db97c7f35451954dba250f9a03ef4ed3f56f
Marcus Buffett <marcusbuffett@me.com>
2015-12-14 08:49:59 -0800

    Updated types, still a work in progress

--- src/Types.hs
+++ src/Types.hs
@@ -1,9 +1,29 @@
 module Types where
 
-data Expr = MyInt Int | 
+import Control.Monad.ST.Strict
+import Data.Map
+
+data MyExpr = MyInt Int | 
             MyString String |
-            MyAdd Expr Expr |
-            MyPrint Expr
-            deriving (Show, Eq)
+            MyPrint MyExpr |
+            MyFunction [String] [MyExpr] |
+            MyBuiltin ([MyExpr] -> MyExpr) |
+            MyVar String MyExpr |
+            MySymbol String |
+            MyBinaryOp (MyExpr -> MyExpr -> MyExpr) |
+            MyUnaryOp (MyExpr -> MyExpr) |
+            MyApplication MyExpr [MyExpr]
+
+instance Show MyExpr where
+  show (MyFunction args lines)         = "Function"
+  show (MyInt x)               = "Integer " ++ show x
+  show (MyString x)            = "String " ++ x
+  show (MyPrint x)             = "Print " ++ show x
+  show (MyVar str expr)        = "Var " ++ str ++ show expr
+  show (MySymbol str)          = "Symbol " ++ str
+  show (MyBuiltin f)           = "Built in! "
+  show (MyBinaryOp f)          = "Binary op"
+  show (MyApplication f xs)    = "Application " ++ show f ++ show xs
 
 type Program = String
+type Context = Map String MyExpr
