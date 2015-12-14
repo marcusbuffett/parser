4e6352142ab9acefc9970fb5ff21119d3fd787f8
Marcus Buffett <marcusbuffett@me.com>
2015-12-14 08:51:36 -0800

    runProgram -> runAST, for testing purposes

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,14 @@ module Main where
 import Types
 import Parser
 import Evaluator
+import Control.Monad.State.Lazy
 
-main = runProgram "STRING \"hi\" + STRING \"aonetuh\""
+main = do
+   runAST [(MyVar "x" (MyInt 4)), MyVar "thingy" (MyApplication (MySymbol "plus") [MyInt 3, MySymbol "x", MyInt 3])]
+   {- runProgram "INT 3 + INT 234" -}
 
-runProgram :: Program -> IO ()
-runProgram = print . fmap evaluate . programToAST
+{- runProgram :: Program -> IO () -}
+{- runProgram = print . fmap ((flip evalStateT) initialContext . evaluate) . programToAST -}
+
+runAST :: [MyExpr] -> IO ()
+runAST ast = print $ foldM (\ctx expr -> liftM snd (runStateT (evaluate expr) ctx)) [initialContext] ast
